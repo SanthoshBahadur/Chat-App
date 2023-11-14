728faa798b77f11d55669e39dbb5b88a6b8e2ab9
Santhosh Bahadur <santhosh.bahadur2@gmail.com>
2023-11-15 03:31:51 +0530

    change host

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://chat-app-steel-psi.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
