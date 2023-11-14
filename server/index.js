c10ffb4a1ffbf4bb29162f10af0b07762529e5f6
Santhosh Bahadur <santhosh.bahadur2@gmail.com>
2023-11-15 03:13:05 +0530

    Uploaded Files

--- server/index.js
+++ server/index.js
@@ -4,27 +4,20 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const messageRoute = require("./routes/messagesRoute");
 const socket = require("socket.io");
-import path from "path"
+
 const app = express();
-import { fileURLToPath } from "url";
-
-
 
 require("dotenv").config();
 // The line require("dotenv").config();
 // is typically used in Node.js applications to load environment variables from a .env file
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'./client/build')))
+
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoute);
 // maybe
 
-app.use('*', function(res, req){
-res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
 
 
 mongoose
@@ -43,8 +36,6 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on ${process.env.PORT}`);
 });
 
-
-
 const io = socket(server, {
   cors: {
     origin: "http://localhost:3000 ",
@@ -57,9 +48,9 @@ global.onlineUsers = new Map();
 
 io.on("connection", (socket) => {
   global.chatSocket = socket;
-    socket.on("add-user", (userId) => {
-      onlineUsers.set(userId, socket.id);
-    });
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
 
   socket.on("send-msg", (data) => {
     const sendUserSocket = onlineUsers.get(data.to);
